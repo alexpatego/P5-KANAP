1d0d722f069a1bde51a196df1a5178a1dc1406a5
Alexandre Patego <pategoalex@gmail.com>
2022-11-04 12:17:44 +0100

    script mis au propre

--- front/js/script.js
+++ front/js/script.js
@@ -1,54 +1,46 @@
-/* On appelle l'api du serveur */
+/* On appelle l'api du serveur pour récupérer les produits */
 
-/* variable locale par fonction */
 fetch("http://localhost:3000/api/products")
     .then(res => res.json())
-    .then(kanap => listProduct(kanap));
-
-    /* fonction principale avec forEach pour chaque image */
-function listProduct(kanap){
-    kanap.forEach(products => {
-        const {_id, imageUrl, altTxt, name, description} = products
-
-        const image = createImage(imageUrl, altTxt)
-    
-        const anchor = createAnchor(_id)
-        const article = document.createElement("article")
-        const h3 = createH3(name)
-        const p = createP(description)
-    
-        appendElementsToArticle(article, [image, h3, p])
-        appendArticleToAnchor(anchor, article)   
-        
+    .then((listProducts) => {
+        kanap(listProducts);
+    })
+/* récupère la réponse en JSON, éléments traités sont ensuite appelés listProducts */
+    .catch((err) => {
+        document.querySelector(".titles").innerHTML = "<h1>erreur 404</h1>"
+        console.log("erreur 404, absence de ressource API:" + err);
     });
+/* dans le cas d'une erreur, fait afficher un titre H1 d'erreur et un console.log d'erreur 404 */ 
+
+/* affichage des produits de l'api sur la page (forEach qui s'occupe de chaque produit pour lui créer ses elements) */
+function kanap(listProducts) { 
+    listProducts.forEach(products =>{
+        const {_id, imageUrl, altTxt, name, description} = products;
+       
+        const anchor = createAnchor(_id); 
+
+        const article = document.createElement("article");
+
+        const image = createImage(imageUrl, altTxt);
+        const h3 = createTitle(name)
+        const p = createDescription(description)
+
+        appendArticleToAnchor(anchor, article);
+        appendElementsToArticle(article, [image, h3, p]);
+    })
 }
 
-    /* append elements to article */
-
-function appendElementsToArticle(article, array) {
-    array.forEach(elements => {
-        article.appendChild(elements)
-    });
-}
-
-/* création balise <a> */
+/* creation de la balise <a> et de son id */
 
 function createAnchor(id) {
-    let anchor = document.createElement("a");
-    anchor.href = "./product.html?id=" + id;
-    return anchor  
+    const anchor = document.createElement("a");
+    anchor.href ="./product.html?id=" + id;
+    return anchor;
 }
 
-/* Append article à la balise <a> */
+/* création des élements enfants d'article qui s'afficheront pour chaque produits */
 
-function appendArticleToAnchor(anchor, article){ 
-    let items = document.getElementById("items")
-        items.appendChild(anchor)
-        anchor.appendChild(article)
-}
-
-/* création des balises enfant de Article */
-
+/* création de la balise <img>, affichage de l'image et de ses élements */
 function createImage(imageUrl, altTxt) {
     const image = document.createElement("img")
     image.src = imageUrl
@@ -56,16 +48,34 @@ function createImage(imageUrl, altTxt) {
     return image
 }
 
-function createH3(name){
+/* création du titre, ajout de sa class */
+function createTitle(name){
     const h3 = document.createElement("h3")
     h3.textContent = name
     h3.classList.add("productName")
     return h3
 }
 
-function createP(description){
+/* création de la description, ajout de sa class */
+function createDescription(description){
     const p = document.createElement("p")
     p.textContent = description
     p.classList.add("productDescription")
     return p
 }
+
+/* fonction qui ajoute la balise <a> à la section et qui ajoute l'article */
+function appendArticleToAnchor(anchor, article) {
+    let items = document.getElementById("items")
+    items.appendChild(anchor)
+    anchor.appendChild(article)
+}
+
+/* fonction qui ajoute les elements à l'article et permet leur affichage */
+
+function appendElementsToArticle(article, array) {
+    array.forEach(elements => {
+        article.appendChild(elements)
+    });
+}
+
